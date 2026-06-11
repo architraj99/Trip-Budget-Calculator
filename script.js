89c26375fa26b0ad0dcc93f15b94d559ff3a92a6
architraj99 <rajarchit99@gmail.com>
2026-06-11 21:53:44 +0530

    Finalized the project...

--- script.js
+++ script.js
@@ -45,6 +45,18 @@ calculateBtn.addEventListener("click", function() {
     localStorage.setItem("savedTrips", JSON.stringify(savedTrips));
 
     displayTrips();
+
+    document.getElementById("destination").value = "";
+    document.getElementById("travelers").value = "";
+    document.getElementById("transport").value = "";
+
+    document.getElementById("hotel").value = "";
+    document.getElementById("food").value = "";
+    
+    document.getElementById("activities").value = "";
+    document.getElementById("other").value = "";
+    document.getElementById("budget").value = "";
+    
 });
 
 function displayTrips() {
